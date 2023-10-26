4b8ec62ad58b50c74b1981628c0dc4707351f077
arifur946954 <arif.bd08060@gmail.com>
2023-10-26 20:08:53 +0600

    add jdbc

--- Security/src/main/java/com/SecurityMVC/Security/security/DemoSecurityConfig.java
+++ Security/src/main/java/com/SecurityMVC/Security/security/DemoSecurityConfig.java
@@ -3,35 +3,20 @@ package com.SecurityMVC.Security.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 public class DemoSecurityConfig {
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails john= User.builder()
-                .username("john")
-                .password("{noop}123456")
-                .roles("EMPLOYEE")
-                .build();
+@Bean
+public UserDetailsManager userDetailsManager(DataSource dataSource){
+    return new JdbcUserDetailsManager(dataSource);
 
-        UserDetails mary= User.builder()
-                .username("mary")
-                .password("{noop}123456")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-
-        UserDetails susan= User.builder()
-                .username("susan")
-                .password("{noop}123456")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john,mary,susan);
-    }
+}
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
         http.authorizeHttpRequests(configurer->
@@ -54,5 +39,28 @@ public class DemoSecurityConfig {
 
         return http.build();
     }
+
+    //    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//        UserDetails john= User.builder()
+//                .username("john")
+//                .password("{noop}123456")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary= User.builder()
+//                .username("mary")
+//                .password("{noop}123456")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//
+//        UserDetails susan= User.builder()
+//                .username("susan")
+//                .password("{noop}123456")
+//                .roles("EMPLOYEE","MANAGER","ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john,mary,susan);
+//    }
 
 }
